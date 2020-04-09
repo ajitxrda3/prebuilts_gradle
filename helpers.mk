5291f87c903e790095971dbaf4e645f8c15ccf7a
Bithium S.A <developer@bithium.com>
2020-04-09 10:27:51 +0100

    CHG: Manage target dependencies outside of `gradle_build` rule.

--- helpers.mk
+++ helpers.mk
@@ -7,10 +7,7 @@ GRADLE_HOME_PATH = $(abspath $(PRODUCT_OUT)/.gradle)
 
 define gradle_build
 
-JAVA_SRCS := $(call all-java-files-under, $(2))
-XML_SRCS : $(shell find $(2) -type f -name *.xml)
-
-$(1): $(JAVA_SRCS) $(XML_SRCS)
+$(1):
 	mkdir -p $(GRADLE_GLOBAL_BUILD_PATH)
 	echo "sdk.dir = $(ANDROID_SDK_ROOT)" > $(2)/local.properties
 	GRADLE_GLOBAL_BUILD_PATH=$(GRADLE_GLOBAL_BUILD_PATH) \
