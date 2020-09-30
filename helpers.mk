1d121ddbdbb71ef88a0ab38d7a5e9f613935e734
Filipe Alves <filipe.alves@bithium.com>
2020-09-30 17:02:05 +0000

    CHG: Ensure `gradle` is installed before trying  to use it.

--- helpers.mk
+++ helpers.mk
@@ -7,7 +7,7 @@ GRADLE_HOME_PATH = $(abspath $(PRODUCT_OUT)/.gradle)
 
 define gradle_build
 
-$(1):
+$(1): | gradle-prebuilt
 	mkdir -p $(GRADLE_GLOBAL_BUILD_PATH)
 	grep -qF 'sdk.dir' $(2)/local.properties || echo "sdk.dir = $(ANDROID_SDK_ROOT)" >> $(2)/local.properties
 	GRADLE_GLOBAL_BUILD_PATH=$(GRADLE_GLOBAL_BUILD_PATH) \
